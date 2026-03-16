bbedcf1bbccc5871ad6aae8cd00cc363e7e490cc
Douglas Lopes <contato.geardev@gmail.com>
2026-03-15 22:46:30 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -19,7 +19,7 @@ describe('SignUp Controller', () => {
 })
 
 describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provided', () => {
+  test('Should return 400 if no email is provided', () => {
     const sut = new SignUpController()
 
     const httpRequest = {
